3be3cfe11ae2f1894ac285de15cdff074d5b35b6
Cvr421 <chandravijayk42187@gmail.com>
2024-11-21 23:41:10 +0530

    setting tabs at footer

---
+++ app/(tabs)/_layout.jsx
@@ -0,0 +1,45 @@
+import { Tabs } from 'expo-router';
+import React from 'react';
+import { Platform } from 'react-native';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
+export default function TabLayout() {
+  // const colorScheme = useColorScheme();
+  return (
+    <Tabs
+    screenOptions={{
+      tabBarStyle: Platform.select({
+        ios: {
+          // Use a transparent background on iOS to show the blur effect
+          position: 'absolute',
+        },
+        default: {},
+      }),
+    }}
+    
+    
+    
+    
+    
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="home" color={color} size={size} />
+          ),
+          
+        }}
+      />
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: 'Profile',
+          tabBarIcon: ({ color, size }) => (
+            <MaterialIcons name="person" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
